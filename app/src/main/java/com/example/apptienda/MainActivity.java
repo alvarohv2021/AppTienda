3050c1c9a68caa9d019d6fb91a65e58ebec4f601
alvarohv2021 <alvarohv2013@gmail.com>
2023-10-10 20:08:03 +0200

    Averiguando como ver imagenes

---
+++ app/src/main/java/com/example/apptienda/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.apptienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void añadir(View view) {
+
+        TextView viewTotalPrecio = findViewById(R.id.precioTotal);
+        TextView viewCantidadTotal = findViewById(R.id.cantidadTotal);
+
+        int totalPrecio = Integer.parseInt(viewTotalPrecio.getText().toString());
+        int cantidadTotal = Integer.parseInt(viewCantidadTotal.getText().toString());
+
+        viewTotalPrecio.setText(totalPrecio);
+        viewCantidadTotal.setText(cantidadTotal+1+"");
+
+    }
+}

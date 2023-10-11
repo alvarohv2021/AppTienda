71bada5755ada2b6ef10f0db8a90903af021203e
alvarohv2021 <alvarohv2013@gmail.com>
2023-10-11 17:24:47 +0200

    App tienda completada

--- app/src/main/java/com/example/apptienda/MainActivity.java
+++ app/src/main/java/com/example/apptienda/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,15 +16,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void añadir(View view) {
-
         TextView viewTotalPrecio = findViewById(R.id.precioTotal);
         TextView viewCantidadTotal = findViewById(R.id.cantidadTotal);
 
-        int totalPrecio = Integer.parseInt(viewTotalPrecio.getText().toString());
+        // Obtener el precio actual del TextView asociado al botón
+        String precioActual = ((TextView) ((ViewGroup) view.getParent()).getChildAt(2)).getText().toString();
+
+        // Eliminar el símbolo de euro (€) y convertir el precio a un número
+        int precioProducto = Integer.parseInt(precioActual.replace("€", ""));
+
+        // Actualizar el total del precio y la cantidad
+        String totalPrecio = viewTotalPrecio.getText().toString();
+        int precioTotalProductos = Integer.parseInt(totalPrecio.replace("€", ""));
         int cantidadTotal = Integer.parseInt(viewCantidadTotal.getText().toString());
 
-        viewTotalPrecio.setText(totalPrecio+1+"");
-        viewCantidadTotal.setText(cantidadTotal+1+"");
+        // Actualizar el total del precio y la cantidad
+        viewTotalPrecio.setText((precioTotalProductos + precioProducto) + "€");
+        // Eliminar el símbolo de euro (€) y convertir el precio a un número
 
+        viewCantidadTotal.setText((cantidadTotal + 1) + "");
     }
+
 }

effd52ac2bd44b0d1834aeb5c8a2f045b967a398
alvarohv2021 <alvarohv2013@gmail.com>
2023-11-08 13:02:43 +0100

    Añadida la funcionalidad de eliminar productos de la lista

--- app/src/main/java/com/example/apptienda/Product_RecyclerViewAdapter.java
+++ app/src/main/java/com/example/apptienda/Product_RecyclerViewAdapter.java
@@ -93,6 +93,7 @@ public class Product_RecyclerViewAdapter extends RecyclerView.Adapter<Product_Re
 
             itemView.setOnLongClickListener(view -> {
                 productList.remove(getAdapterPosition());
+                notifyDataSetChanged();
                 return true;
             });
 
